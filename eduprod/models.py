a24a4a0431939d919350706c9662d550e4f1ea49
receedinglines <sayan.jain@student.uts.edu.au>
2024-03-19 16:38:00 +1100

    Initial commit

---
+++ eduprod/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    answer_text = models.TextField()
+
+    def __str__(self):
+        return self.question_text
---
+++ eduprod/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.core import serializers
+from django.shortcuts import render
+from .models import Question
+
+def index(request):
+    questions = Question.objects.all()
+    questions_json = serializers.serialize('json', questions)
+    return render(request, 'eduprod/index.html', {'questions_json': questions_json})
